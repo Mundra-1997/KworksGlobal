e2c59e6f2c379869155facc816773d12983e1100
Mundra-1997 <darshandm28@gmail.com>
2023-11-28 00:28:10 +0530

    commit

---
+++ src/components/Banner.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Banner.css'
+const Banner = () => {
+  return (
+    <div className='main-div'>
+      <div className='banner-div'>
+         <div className='banner-heading'>At K Works Global Solutions,<br/>
+         we understand the evolving needs of businesses and offer a <br/>
+         tailored FaaS solution to enhance your financial operations.
+</div>
+</div>
+<div className='banner-subheading'>
+            <h1>WE WORKS THE <span style={{color:'white'}}>NUMBERS!</span></h1>
+            <h1>SO YOU CAN DO <span style={{color:'white'}}>WONDERS!</span></h1>
+            </div>
+
+      
+    </div>
+  )
+}
+
+export default Banner
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,11 @@ import Services from './Services';
 import Values from './Values';
 import WhyUs from './WhyUs';
 import ProblemSolution from './ProblemSolution';
+import Banner from './Banner';
 const Home = () => {
   return (
     <div>
+      <Banner/>
           <Services />
       <Values/>
       <WhyUs/>
