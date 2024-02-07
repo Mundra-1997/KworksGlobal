0c7cfd11a1b40584d6f1328dfd13faa69ab47d74
Anshul <100751856+hi-anshul@users.noreply.github.com>
2024-02-07 11:19:51 +0530

    Fixing Favicon Path

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -8,7 +8,7 @@ import Favicon from "react-favicon";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Favicon url="%PUBLIC_URL%/favicon.ico" />
+    <Favicon url="/favicon.ico" />
       <App />
   </React.StrictMode>,
   document.getElementById('root')
